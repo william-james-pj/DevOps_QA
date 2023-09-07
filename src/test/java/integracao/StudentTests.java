cab5960013c1cef41605a2b9d017135003067d1a
William James <52788612+william-james-pj@users.noreply.github.com>
2023-09-07 10:15:09 -0300

    feat: starting project

---
+++ src/test/java/integracao/StudentTests.java
@@ -0,0 +1,15 @@
+package integracao;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StudentTests {
+	
+	Student student;
+
+	@Test
+	public void test(){
+		student = new Student();
+	}
+}
